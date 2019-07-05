d52f68e4431cfa3420b53cb53bdbe8db0423966b
William Lindner <wlindner522@gmail.com>
2019-07-04 17:11:25 -0700

    Initial Commit

---
+++ app/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  let counter = 666;
+  let message;
+  $: message =
+    counter <= 0
+      ? "Hoorraaay! You unlocked the Svelte-Native clicker achievement!"
+      : `${counter} taps left`;
+
+  const onTap = () => counter--;
+</script>
+
+<page class="page">
+  <actionBar title="My App" icon="" class="action-bar" />
+  <stackLayout class="p-20">
+    <label text="Tap the button" class="h1 text-center" />
+    <button text="TAP" on:tap={onTap} class="btn btn-primary btn-active" />
+    <label text={message} class="h2 text-center" textWrap="true" />
+  </stackLayout>
+</page>
