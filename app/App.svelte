9859381bdda4b4260dc3889380a0f12e2abf5511
William Lindner <wlindner522@gmail.com>
2019-07-04 17:51:46 -0700

    Allow the user to input a token

--- app/App.svelte
+++ app/App.svelte
@@ -1,16 +1,20 @@
 <script>
   import * as appSettings from "tns-core-modules/application-settings";
 
+  import TokenPage from "./TokenPage";
+
   const token = appSettings.getString("token");
+
+  console.log(token);
 </script>
 
 <page>
   <actionBar title="Pi-Hole Remote" />
   <stackLayout class="p-20">
     {#if !token}
-      <label text="Token Not Found" class="h1 text-center" />
+      <TokenPage {token} />
     {:else}
-      <label text="Token Found" class="h1 text-center" />
+      <label text="Token Definitely Found" class="h1 text-center" />
     {/if}
   </stackLayout>
 </page>
---
+++ app/TokenPage.svelte
@@ -0,0 +1,19 @@
+<script>
+  import * as appSettings from "tns-core-modules/application-settings";
+
+  export let token;
+
+  const saveToken = () => {
+    appSettings.setString("token", token);
+  };
+</script>
+
+<page>
+  <textField
+    hint="Enter token"
+    bind:text={token}
+    returnKeyType="done"
+    on:returnPress={saveToken}
+    autocorrect="false"
+    class="input input-border" />
+</page>
