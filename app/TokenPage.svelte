14da2f1c12829b8f84ad40d78a975924ef64eb5a
William Lindner <wlindner522@gmail.com>
2019-07-14 21:40:24 -0700

    Use a barcode scanner to read the API QR token
    - use NSCameraUsageDescription to justify our use of the camera

--- app/TokenPage.svelte
+++ app/TokenPage.svelte
@@ -1,14 +1,33 @@
 <script>
+  import { BarcodeScanner } from "nativescript-barcodescanner";
   import * as appSettings from "tns-core-modules/application-settings";
   import { token } from "./Store.js";
 
   let text = $token;
+  const barcodescanner = new BarcodeScanner();
 
   const saveToken = () => {
     if (text.length == 64) {
       $token = text;
       appSettings.setString("token", $token);
     }
+  };
+
+  const openBarcodeScanner = () => {
+    barcodescanner
+      .scan({
+        formats: "QR_CODE",
+        beepOnScan: false,
+        openSettingsIfPermissionWasPreviouslyDenied: true // On iOS you can send the user to the settings app if access was previously denied
+      })
+      .then(
+        result => {
+          text = result.text;
+        },
+        errorMessage => {
+          console.log("No scan. " + errorMessage);
+        }
+      );
   };
 </script>
 
@@ -29,14 +48,16 @@
     color: #2847d2;
   }
 
-  button.save {
+  button {
     background-color: black;
-    height: 88px;
+    height: 176px;
+    margin-bottom: 10px;
   }
 </style>
 
 <stackLayout>
   <label text="Token" />
   <textView hint="Enter token" bind:text maxLength="64" autocorrect="false" />
+  <button class="scan" text="QR Scan" on:tap={openBarcodeScanner} />
   <button class="save" text="Save" on:tap={saveToken} />
 </stackLayout>
